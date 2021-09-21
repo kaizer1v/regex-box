75f33ac83ea9d09b739a7b8cfd7332b1532a45cb
kaizer1v <kaizer1v@gmail.com>
2021-09-21 16:00:25 +0530

    cycle through matching results

--- src/js/content.js
+++ src/js/content.js
@@ -3,8 +3,6 @@ let gl;   // declare a global variable `gl`
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   let regex
 
-  console.log(req)
-
   try {
     regex = new RegExp(req.input);
   } catch(e) {
@@ -12,21 +10,27 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
     return false
   }
 
+  // if a new search is required, then
   if(req['new_search']) {
     // remove old highlights & highlight new matches again
     highlight_remove('mark.highlighted, mark.selected')
     gl = highlight(regex, document.getElementsByTagName('body')[0])
     console.log(gl)
+
+  // if a new search is not required, then
   } else {
-    // highlight next item, based on index
-    gl['index'] += 1
-    el_select(gl['index'], gl['arr'])
+    if(req['next_prev']) {
+      // select next matched item, based on index
+      gl['index'] = el_select(gl['index'] += 1, gl['arr'])
+    } else {
+      gl['index'] = el_select(gl['index'] -= 1, gl['arr'])
+    }
   }
 
   sendResponse({
     'arr': gl,
     'results_index': gl['index'],
-    'results_total': gl['total']
+    'results_total': gl['total'] - 1      // since index starts from 0
   })
 })
 
@@ -35,12 +39,12 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
  *
  * Params
  *    `regex`: a regex object
- *    `el`   : the element within which the search needs to be conducted
+ *    `el`   : the element within which the regex search needs to be conducted
  *
  * Returns
- *    `results_total`: total number of matched results
- *    `results_index`: current highlighted result out of total
- *    ``
+ *    `total`: total number of matched results
+ *    `index`: current highlighted result out of total
+ *    `arr`:   an array of all matched elements
  */
 let highlight = (regex, el=document.getElementsByTagName('body')[0]) => {
   let total = 0,
@@ -119,16 +123,26 @@ let highlight_remove = (q) => {
  * Select regex matched element
  */
 let el_select = (index, highlighted_els) => {
-  // default them back
+  // unselect all highlighted elements
   highlighted_els.forEach((el) => {
     el.style.backgroundColor = 'red'
     el.classList.remove('selected')
   })
 
+  // if index is out of bounds, then cycle back
+  if(index < 0) {
+    index = highlighted_els.length
+  }
+  if(index > highlighted_els.length - 1) {
+    index = 0
+  }
+
+  // only change the background of the selected node
   highlighted_els[index].style.backgroundColor = 'green'
   highlighted_els[index].classList.add('selected')
 
   el_scroll_to(highlighted_els[index])
+  return index
 }
 
 
@@ -141,301 +155,4 @@ let el_scroll_to = (el) => {
     block: 'center',
     inline: 'center'
   })
-  // let top = el.documentOffsetTop() - (window.innerHeight / 2)
-  // window.scrollTo(0, Math.max(top, window.pageYOffset - (window.innerHeight / 2)))
 }
-
-/**
- * Select next matched element
- */
-let result_next = (highlightedColor, selectedColor) => {
-  el_select(highlightedColor, selectedColor, true)
-}
-
-/**
- * Select previous matched element
- */
-let result_prev = (highlightedColor, selectedColor) => {
-  el_select(highlightedColor, selectedColor, false)
-}
-
-
-
-
-// /*** CONSTANTS ***/
-// var ELEMENT_NODE_TYPE = 1;
-// var TEXT_NODE_TYPE = 3;
-// var UNEXPANDABLE = /(script|style|svg|audio|canvas|figure|video|select|input|textarea)/i;
-// var HIGHLIGHT_TAG = 'highlight-tag';
-// var HIGHLIGHT_CLASS = 'highlighted';
-// var SELECTED_CLASS = 'selected';
-// var DEFAULT_MAX_RESULTS = 500;
-// var DEFAULT_HIGHLIGHT_COLOR = '#ffff00';
-// var DEFAULT_SELECTED_COLOR = '#ff9900';
-// var DEFAULT_TEXT_COLOR = '#000000';
-// var DEFAULT_CASE_INSENSITIVE = false;
-// /*** CONSTANTS ***/
-
-// /*** GLOBAL VARIABLE ***/
-// var searchInfo;
-
-
-// /*** LIBRARY FUNCTIONS ***/
-// Element.prototype.documentOffsetTop = () => {
-//   return this.offsetTop + ( this.offsetParent ? this.offsetParent.documentOffsetTop() : 0 );
-// };
-// Element.prototype.visible = function() {
-//   return (!window.getComputedStyle(this) || window.getComputedStyle(this).getPropertyValue('display') == '' ||
-//          window.getComputedStyle(this).getPropertyValue('display') != 'none')
-// }
-// /*** LIBRARY FUNCTIONS ***/
-
-
-// /*** FUNCTIONS ***/
-// /* Initialize search information for this tab */
-// function initSearchInfo(pattern) {
-//   var pattern = typeof pattern !== 'undefined' ? pattern : '';
-//   searchInfo = {
-//     regexString : pattern,
-//     selectedIndex : 0,
-//     highlightedNodes : [],
-//     length : 0
-//   }
-// }
-
-// /* Send message with search information for this tab */
-// function returnSearchInfo(cause) {
-//   chrome.runtime.sendMessage({
-//     'message' : 'returnSearchInfo',
-//     'regexString' : searchInfo.regexString,
-//     'currentSelection' : searchInfo.selectedIndex,
-//     'numResults' : searchInfo.length,
-//     'cause' : cause
-//   })
-// }
-
-// /* Check if the given node is a text node */
-// function isTextNode(node) {
-//   return node && node.nodeType === TEXT_NODE_TYPE;
-// }
-
-// /* Check if the given node is an expandable node that will yield text nodes */
-
-// /* Highlight all text that matches regex */
-// function highlight(regex, highlightColor, selectedColor, textColor, maxResults) {
-//   function highlightRecursive(node) {
-//     if(searchInfo.length >= maxResults) {
-//       return;
-//     }
-//     if(isTextNode(node)) {
-//       var index = node.data.search(regex);
-//       if(index >= 0 && node.data.length > 0) {
-//         var matchedText = node.data.match(regex)[0];
-//         var matchedTextNode = node.splitText(index);
-//         matchedTextNode.splitText(matchedText.length);
-//         var spanNode = document.createElement(HIGHLIGHT_TAG);
-//         spanNode.className = HIGHLIGHT_CLASS;
-//         spanNode.style.backgroundColor = highlightColor;
-//         spanNode.style.color = textColor;
-//         spanNode.appendChild(matchedTextNode.cloneNode(true));
-//         matchedTextNode.parentNode.replaceChild(spanNode, matchedTextNode);
-//         searchInfo.highlightedNodes.push(spanNode);
-//         searchInfo.length += 1;
-//         return 1;
-//       }
-//     } else if(isExpandable(node)) {
-//         var children = node.childNodes;
-//         for(var i = 0; i < children.length; ++i) {
-//           var child = children[i];
-//           i += highlightRecursive(child);
-//         }
-//     }
-//     return 0;
-//   }
-//   highlightRecursive(document.getElementsByTagName('body')[0]);
-// };
-
-// /* Remove all highlights from page */
-// function removeHighlight() {
-//   while (node = document.body.querySelector(HIGHLIGHT_TAG + '.' + HIGHLIGHT_CLASS)) {
-//     node.outerHTML = node.innerHTML;
-//   }
-//     while (node = document.body.querySelector(HIGHLIGHT_TAG + '.' + SELECTED_CLASS)) {
-//     node.outerHTML = node.innerHTML;
-//   }
-// };
-
-// /* Scroll page to given element */
-// let scrollToElement = (element) => {
-//     element.scrollIntoView()
-//     let top = element.documentOffsetTop() - (window.innerHeight / 2)
-//     window.scrollTo(0, Math.max(top, window.pageYOffset - (window.innerHeight / 2)))
-// }
-
-// /**
-//  *  Select first regex match on page
-//  * config = {
-//  *   selected_class: 'selected'
-//  *   selected_color: ''
-//  * }
-//  */
-// let selectFirstNode = (selectedColor, selectedClass) => {
-//   if(searchInfo.length > 0) {
-//     searchInfo.highlightedNodes[0].className = selectedClass
-//     searchInfo.highlightedNodes[0].style.backgroundColor = selectedColor
-
-//     parentNode = searchInfo.highlightedNodes[0].parentNode
-//     if(parentNode.nodeType === 1) {
-//       parentNode.focus()
-//     } else if (parentNode.parentNode.nodeType == 1) {
-//       parentNode.parentNode.focus()
-//     }
-//     scrollToElement(searchInfo.highlightedNodes[0]);
-//   }
-// }
-
-// /* Helper for selecting a regex matched element */
-// function selectNode(highlightedColor, selectedColor, getNext) {
-//   var length = searchInfo.length;
-//   if(length > 0) {
-//     searchInfo.highlightedNodes[searchInfo.selectedIndex].className = HIGHLIGHT_CLASS;
-//     searchInfo.highlightedNodes[searchInfo.selectedIndex].style.backgroundColor = highlightedColor;
-//       if(getNext) {
-//         if(searchInfo.selectedIndex === length - 1) {
-//           searchInfo.selectedIndex = 0;
-//         } else {
-//           searchInfo.selectedIndex += 1;
-//         }
-//       } else {
-//         if(searchInfo.selectedIndex === 0) {
-//           searchInfo.selectedIndex = length - 1;
-//         } else {
-//           searchInfo.selectedIndex -= 1;
-//         }
-//       }
-//     searchInfo.highlightedNodes[searchInfo.selectedIndex].className = SELECTED_CLASS;
-//     searchInfo.highlightedNodes[searchInfo.selectedIndex].style.backgroundColor = selectedColor;
-//     parentNode = searchInfo.highlightedNodes[searchInfo.selectedIndex].parentNode;
-//     if (parentNode.nodeType === 1) {
-//       parentNode.focus();
-//     } else if (parentNode.parentNode.nodeType == 1) {
-//       parentNode.parentNode.focus();
-//     }
-//     returnSearchInfo('selectNode');
-//     scrollToElement(searchInfo.highlightedNodes[searchInfo.selectedIndex]);
-//   }
-// }
-// /* Forward cycle through regex matched elements */
-// function selectNextNode(highlightedColor, selectedColor) {
-//   selectNode(highlightedColor, selectedColor, true);
-// }
-
-// /* Backward cycle through regex matched elements */
-// function selectPrevNode(highlightedColor, selectedColor) {
-//   selectNode(highlightedColor, selectedColor, false);
-// }
-
-// /* Validate that a given pattern string is a valid regex */
-// function validateRegex(pattern) {
-//   try{
-//     var regex = new RegExp(pattern);
-//     return regex;
-//   } catch(e) {
-//     return false;
-//   }
-// }
-
-// /**
-//  * MAINNNNNNNNNNNNNNNNNNN
-//  */
-// /* Find and highlight regex matches in web page from a given regex string or pattern */
-// function search(regexString, configurationChanged) {
-//   var regex = validateRegex(regexString);
-//   if (regex && regexString != '' && (configurationChanged || regexString !== searchInfo.regexString)) { // new valid regex string
-//     removeHighlight();
-//     chrome.storage.local.get({
-//       'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
-//       'selectedColor' : DEFAULT_SELECTED_COLOR,
-//       'textColor' : DEFAULT_TEXT_COLOR,
-//       'maxResults' : DEFAULT_MAX_RESULTS,
-//       'caseInsensitive' : DEFAULT_CASE_INSENSITIVE},
-//       function(result) {
-//         initSearchInfo(regexString);
-//         if(result.caseInsensitive){
-//           regex = new RegExp(regexString, 'i');
-//         }
-//         highlight(regex, result.highlightColor, result.selectedColor, result.textColor, result.maxResults);
-//         selectFirstNode(result.selectedColor, SELECTED_CLASS)
-//         returnSearchInfo('search');
-//       }
-//     );
-//   } else if (regex && regexString != '' && regexString === searchInfo.regexString) { // elements are already highlighted
-//     chrome.storage.local.get({
-//       'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
-//       'selectedColor' : DEFAULT_SELECTED_COLOR},
-//       function(result) {
-//         selectNextNode(result.highlightColor, result.selectedColor);
-//       }
-//     );
-//   } else { // blank string or invalid regex
-//     removeHighlight();
-//     initSearchInfo(regexString);
-//     returnSearchInfo('search');
-//   }
-// }
-// /*** FUNCTIONS ***/
-
-// ** LISTENERS **
-// /* Received search message, find regex matches */
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if ('search' == request.message) {
-//     search(request.regexString, request.configurationChanged);
-//   }
-//   /* Received selectNextNode message, select next regex match */
-//   else if ('selectNextNode' == request.message) {
-//     chrome.storage.local.get({
-//       'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
-//       'selectedColor' : DEFAULT_SELECTED_COLOR
-//       },
-//       function(result) {
-//         selectNextNode(result.highlightColor, result.selectedColor);
-//       }
-//     );
-//   }
-//   /* Received selectPrevNode message, select previous regex match */
-//   else if ('selectPrevNode' == request.message) {
-//     chrome.storage.local.get({
-//       'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
-//       'selectedColor' : DEFAULT_SELECTED_COLOR
-//       },
-//       function(result) {
-//         selectPrevNode(result.highlightColor, result.selectedColor);
-//       }
-//     );
-//   }
-//   else if ('copyToClipboard' == request.message) {
-//     var clipboardHelper = document.createElement('textarea');
-//     try {
-//       var text = searchInfo.highlightedNodes.map(function (n) {
-//         return n.innerText;
-//       }).join('\n');
-//       clipboardHelper.appendChild(document.createTextNode(text));
-//       document.body.appendChild(clipboardHelper);
-//       clipboardHelper.select();
-//       document.execCommand('copy');
-//     } finally {
-//       document.body.removeChild(clipboardHelper);
-//     }
-//   }
-//   /* Received getSearchInfo message, return search information for this tab */
-//   else if ('getSearchInfo' == request.message) {
-//     sendResponse({message: "I'm alive!"});
-//     returnSearchInfo('getSearchInfo');
-//   }
-// });
-// /*** LISTENERS ***/
-
-
-// /*** INIT ***/
-// initSearchInfo();
-// /*** INIT ***/
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,10 +1,7 @@
-/**
- * Fire event whenever user types inside textbox
- */
 let map = []
-var input_changed = false
+var input_changed = false       // global modifiable variable
 
-// Watch on `keyup` as well as the `keydown` event
+// Keep watching `keydown` event on `inputRegex` input box
 onkeydown = (e) => {
   map[e.keyCode] = e.type === 'keydown'
 
@@ -15,7 +12,8 @@ onkeydown = (e) => {
       map[13] = false
       send_message({
         'input': e.target.value,
-        'new_search': input_changed
+        'new_search': input_changed,
+        'next_prev': true
       })
 
     // NOT changed text + pressed shift & enter
@@ -25,7 +23,8 @@ onkeydown = (e) => {
 
       send_message({
         'input': e.target.value,
-        'new_search': input_changed
+        'new_search': input_changed,
+        'next_prev': false
       })
     } else {
       map[13] = false
@@ -37,7 +36,7 @@ onkeydown = (e) => {
 
 let send_message = (params) => {
   chrome.tabs.query({ 'active': true, 'currentWindow': true }, (currTab) => {
-    // update params with current tab info
+    // update params object with all current tab info
     params['tab'] = currTab[0]
 
     // send params to `content.js` & expect a response
@@ -48,303 +47,3 @@ let send_message = (params) => {
     })
   })
 }
-
-// /*** DEFAULTS ***/
-// const DEFAULTS = {
-//   'error_color': '#F8D7DA',
-//   'white_color': '#ffffff',
-//   'error_text': "Content script was not loaded on this url or please wait for the page to load.",
-//   'history_is_empty_text': "Search history is empty.",
-//   'clear_all_history_text': "Clear History",
-//   'max_history_length': 30
-// }
-
-// /*** VARIABLES ***/
-// // let DEFAULT_CASE_INSENSITIVE = false
-// let sentInput = false,          // ???
-//     searchHistory = null,
-//     maxHistoryLength = DEFAULTS['max_history_length']
-
-// /*** ELEMENTS ***/
-// const txt_regex = document.getElementById('inputRegex'),
-//       num_results = document.getElementById('numResults')
-
-// /**
-//  * Initialisation - run when the extension is opened
-//  */
-// /* Received returnSearchInfo message, populate popup UI */
-// chrome.runtime.onMessage.addListener((request, sender) => {
-//   console.log('received', request, sender);
-//   if(request.message == 'returnSearchInfo') {
-//     // update the results `0 of 0` text
-//     let sel = (request.numResults > 0) ? request.currentSelection + 1 : request.currentSelection
-//     num_results.textContent = `${sel} of ${request.numResults}`
-
-//     if(!sentInput) {
-//       txt_regex.value = request.regexString
-//     }
-//     if(request.numResults > 0 && request.cause == 'selectNode') {
-//       addToHistory(request.regexString);
-//     }
-//     if(request.regexString !== txt_regex.value) {
-//       console.log('yahaan seee');
-//       search()
-//     }
-//   }
-// });
-
-// /**
-//  * Input box keydown event listener
-//  */
-// // txt_regex.addEventListener('keydown', (e) => {
-// //   if(e.key == 'Enter' && e.key != 'Shift') {                // ENTER
-// //     e.preventDefault()
-// //     next_prev(true)
-// //   } else if (e.key == 'Shift' && (e.key == 'Enter')) {      // SHIFT + ENTER
-// //     next_prev(false)
-// //   } else if(e.key == 'Escape') {
-// //     // TODO: remove highlights from page
-// //   } else {
-// //     search()                                                // any char other than SHIFT or ENTER
-// //   }
-// // }, true)
-
-
-// /**
-//  * Sets regbex text box's state (error or normal) based on validity
-//  */
-// let set_status_regbox = (valid) => {
-//   txt_regex.parentNode.style.backgroundColor = (!valid) ? DEFAULTS['error_color'] : DEFAULTS['white_color']
-//   return
-// }
-
-// /**
-//  * Fire the query to search pattern on browser page
-//  */
-// let search = (changed=undefined) => {
-//   console.log('yo o yo')
-//   // search query corrent + incorrect ones
-//   chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
-//     if(typeof tabs[0].id != 'undefined') {
-//       chrome.tabs.sendMessage(tabs[0].id, {
-//         'message': 'search',
-//         'regexString': txt_regex.value,
-//         'configurationChanged': changed,
-//         'getNext': true
-//       })
-//       sentInput = true
-//     }
-//     // chrome.storage.local.set({lastSearch: txt_regex.value});
-//   });
-
-//   set_status_regbox(is_valid_regex(txt_regex.value))   // set text box state (error or normal)
-// }
-
-// /*** FUNCTIONS ***/
-// /* Validate that a given pattern string is a valid regex */
-// let is_valid_regex = (pattern) => {
-//   try {
-//     new RegExp(pattern)
-//     return true
-//   } catch(e) {
-//     console.error(e)
-//     return false
-//   }
-// }
-
-// /**
-//  * Select next or previous based on `true` or `false`
-//  */
-// let next_prev = (next=true) => {
-//   let msg = (next) ? 'selectNextNode' : 'selectPrevNode'
-
-//   chrome.tabs.query({
-//     'active': true,
-//     'currentWindow': true
-//   }, (tabs) => {
-//     if(typeof tabs[0].id != 'undefined') {
-//       chrome.tabs.sendMessage(tabs[0].id, {
-//         'message': msg
-//       })
-//     }
-//   })
-//   return true
-// }
-
-
-// let createHistoryLineElement = (text) => {
-//   var deleteEntrySpan = document.createElement('span');
-//   deleteEntrySpan.className = 'historyDeleteEntry fas fa-times'
-//   deleteEntrySpan.addEventListener('click', () => {
-//     for(let i = searchHistory.length - 1; i >= 0; i--) {
-//       if(searchHistory[i] == text) {
-//         searchHistory.splice(i, 1);
-//       }
-//     }
-//     chrome.storage.local.set({searchHistory: searchHistory});
-//     updateHistoryDiv();
-//   });
-//   var linkSpan = document.createElement('span');
-//   linkSpan.className = 'historyLink'
-//   linkSpan.textContent = text;
-//   linkSpan.addEventListener('click', () => {
-//     if (txt_regex.value !== text) {
-//       txt_regex.value = text;
-//       search();
-//       txt_regex.focus();
-//     }
-//   });
-//   var lineDiv = document.createElement('li');
-//   lineDiv.appendChild(deleteEntrySpan);
-//   lineDiv.appendChild(linkSpan);
-//   return lineDiv;
-// }
-
-// let updateHistoryDiv = () => {
-//   let historyDiv = document.getElementById('history');
-//   if(historyDiv) {
-//     historyDiv.innerHTML = '';
-
-//     // default history is empty message
-//     let span = document.createElement('span');
-//     span.className = 'historyIsEmptyMessage';
-//     span.textContent = DEFAULTS['history_is_empty_text'];
-//     historyDiv.appendChild(span);
-
-//     if(searchHistory.length != 0) {
-//       historyDiv.innerHTML = '';
-//       for(let i = searchHistory.length - 1; i >= 0; i--) {
-//         historyDiv.appendChild(createHistoryLineElement(searchHistory[i]));
-//       }
-
-//       // create `Clear History` button
-//       let clearButton = document.createElement('a');
-//       clearButton.href = '#';
-//       clearButton.type = 'button';
-//       clearButton.textContent = DEFAULTS['clear_all_history_text'];
-//       clearButton.className = 'clearHistoryButton';
-//       clearButton.addEventListener('click', clearSearchHistory);
-//       historyDiv.appendChild(clearButton);
-//     }
-//   }
-// }
-
-// /**
-//  * adds search items to local storage list (after user presses enter)
-//  */
-// let addToHistory = (regex) => {
-//   if(regex && searchHistory !== null) {
-//     if(searchHistory.length == 0 || searchHistory[searchHistory.length - 1] != regex) {
-//       searchHistory.push(regex);
-//     }
-//     for(let i = searchHistory.length - 2; i >= 0; i--) {
-//       if(searchHistory[i] == regex) {
-//         searchHistory.splice(i, 1);
-//       }
-//     }
-//     if(searchHistory.length > maxHistoryLength) {
-//       searchHistory.splice(0, searchHistory.length - maxHistoryLength);
-//     }
-//     chrome.storage.local.set({searchHistory: searchHistory});
-//     updateHistoryDiv();
-//   }
-// }
-
-// /**
-//  * removes list of history items
-//  */
-// let clearSearchHistory = () => {
-//   searchHistory = [];
-//   chrome.storage.local.set({searchHistory: searchHistory});
-//   updateHistoryDiv();
-// }
-
-
-// /*** LISTENERS ***/
-// document.getElementById('next').addEventListener('click', () => {
-//   next_prev(true)
-// })
-
-// document.getElementById('prev').addEventListener('click', () => {
-//   next_prev(false)
-// })
-
-// document.getElementById('show-history').addEventListener('click', () => {
-//   let makeVisible = document.getElementById('history').style.display == 'none'
-//   document.getElementById('history').style.display = makeVisible ? 'block' : 'none'
-//   document.getElementById('show-history').className = (makeVisible) ? 'selected' : ''
-// })
-
-// document.getElementById('cheatsheet').addEventListener('click', () => {
-//   let makeVisible = document.getElementById('cheatsheet_content').style.display == 'none'
-//   document.getElementById('cheatsheet_content').style.display = makeVisible ? 'block' : 'none'
-//   document.getElementById('cheatsheet').className = (makeVisible) ? 'selected' : ''
-// })
-
-// document.getElementById('flags').addEventListener('click', () => {
-//   let makeVisible = document.getElementById('flag_options').style.display == 'none'
-//   document.getElementById('flag_options').style.display = makeVisible ? 'block' : 'none'
-//   document.getElementById('flags').className = (makeVisible) ? 'selected' : ''
-// })
-
-// // document.getElementById('insensitive').addEventListener('click', () => {
-// //   let is_on = (e.target.dataset.on === 'true') ? true : false
-// //   e.target.setAttribute('data-on', !is_on)
-// //   e.target.className = (is_on) ? 'selected' : ''
-// //   chrome.storage.local.set({ caseInsensitive: is_on })
-// //   search(true)
-// // })
-
-// document.getElementById('copy-to-clipboard').addEventListener('click', () => {
-//   chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
-//     if(typeof tabs[0].id != 'undefined') {
-//       chrome.tabs.sendMessage(tabs[0].id, {
-//         'message': 'copyToClipboard'
-//       })
-//     }
-//   })
-// })
-
-// /*** LISTENERS ***/
-
-// /*** INIT ***/
-// /* Retrieve from storage whether we should use instant results or not */
-// chrome.storage.local.get({
-//   'instantResults' : true,
-//   'maxHistoryLength' : DEFAULTS['max_history_length'],
-//   'searchHistory' : null
-//   // 'isSearchHistoryVisible' : false
-// }, (result) => {
-//   let elem = (result.instantResults) ? 'input' : 'change'
-//   txt_regex.addEventListener(elem, () => {
-//     search()
-//   })
-
-//   if(result.maxHistoryLength) {
-//     maxHistoryLength = result.maxHistoryLength;
-//   }
-//   if(result.searchHistory) {
-//     searchHistory = result.searchHistory.slice(0);
-//   } else {
-//     searchHistory = [];
-//   }
-//   document.getElementById('history').style.display = result.isSearchHistoryVisible ? 'block' : 'none';
-//   document.getElementById('show-history').className = (result.isSearchHistoryVisible) ? 'selected' : '';
-//   updateHistoryDiv();
-// })
-
-// /* Get search info if there is any */
-// chrome.tabs.query({
-//   'active': true,
-//   'currentWindow': true
-// }, (tabs) => {
-//   if(typeof tabs[0].id == 'undefined') {
-//     chrome.tabs.sendMessage(tabs[0].id, {
-//       'message' : 'getSearchInfo'
-//     }, (resp) => {
-//       if(!resp) {
-//         document.getElementById('error').textContent = DEFAULTS['error_text']
-//       }
-//     });
-//   }
-// })
